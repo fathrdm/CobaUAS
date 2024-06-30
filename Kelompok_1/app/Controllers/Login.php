217264e11722477aff04bb281808223ba0820f9f
nanutt <najahmaisya03@gmail.com>
2024-06-30 16:03:28 +0800

    first commit

---
+++ Kelompok_1/app/Controllers/Login.php
@@ -0,0 +1,54 @@
+<?php
+namespace App\Controllers;
+use CodeIgniter\Controller;
+use App\Models\AkunModel;
+class Login extends Controller
+{
+    public function index()
+    {
+        helper(['form']);
+        echo view('loginft');
+    }
+    public function auth()
+    {
+        $session = session();
+        $model = new AkunModel();
+        $nama = $this->request->getPost('nama');
+        $password = $this->request->getPost('password');
+        $data = $model->get_data($nama, $password);
+        if (empty($nama) || empty($password)) {
+            $session->setFlashdata('msg', 'Harap lengkapi semua kolom');
+            return redirect()->to('/login');
+        }
+        if ($data) {
+            $ses_data = [
+                'nama' => $data['nama'],
+                'password' => $data['password'],
+                'logged_in' => TRUE
+            ];
+            $session->set($ses_data);
+            return view('dashboard');
+        } else {
+            $session->setFlashdata('msg', 'Username, Email, atau Password Salah');
+            return redirect()->to('/login');
+        }
+        
+    }
+
+    public function logout()
+    {
+        $session = session();
+        $session->destroy();
+        return redirect()->to('/login');
+    }
+}
+?>
+
+
+
+
+
+
+
+
+
---
+++ Kelompok_1/app/Database/Migrations/2024-06-29-101714_Tabelpengajuan.php
@@ -0,0 +1,31 @@
+<!-- <?php
+// namespace App\Database\Migrations;
+
+// use CodeIgniter\Database\Migration;
+
+// class Tabelpengajuan extends Migration
+// {
+//     public function up()
+//     {
+//         $fields = [
+//             'tanggal_pengajuan' => [
+//                 'type'    => 'DATETIME',
+//                 'null'    => false,
+//                 'default' => 'CURRENT_TIMESTAMP',
+//             ],
+//         ];
+//         $this->forge->modifyColumn('tanyaft', $fields);
+//     }
+
+//     public function down()
+//     {
+//         $fields = [
+//             'tanggal_pengajuan' => [
+//                 'type' => 'DATETIME',
+//                 'null' => true,
+//             ],
+//         ];
+//         $this->forge->modifyColumn('tanyaft', $fields);
+//     }
+// }
+?> -->
